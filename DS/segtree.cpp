e554d74fcf8bde6bb4d225adb2070d4ded03843f
Iheb Haboubi <iheb.haboubi56@gmail.com>
2021-02-25 11:17:47 +0100

    Generalize function

--- DS/segtree.cpp
+++ DS/segtree.cpp
@@ -18,14 +18,16 @@ const int N = 2e5;
 int t[2 * N];
 int n;
 
+inline int f(int x, int y) { return x + y; }
+
 void build()
 {
-    for (int i = n - 1; i > 0; i--) t[i] = t[i << 1] + t[i << 1 | 1];
+    for (int i = n - 1; i > 0; i--) t[i] = f(t[i << 1], t[i << 1 | 1]);
 }
 
 void update(int p, int value)
 {
-    for (t[p += n] = value; p > 1; p >>= 1) t[p >> 1] = t[p] + t[p ^ 1];
+    for (t[p += n] = value; p > 1; p >>= 1) t[p >> 1] = f(t[p], t[p ^ 1]);
 }
 
 int query(int l, int r)
@@ -33,8 +35,8 @@ int query(int l, int r)
     int ans = 0;
     for (l += n, r += n; l < r; l >>= 1, r >>= 1)
     {
-        if (l & 1) ans += t[l++];
-        if (r & 1) ans += t[--r];
+        if (l & 1) ans = f(ans, t[l++]);
+        if (r & 1) ans = f(ans, t[--r]);
     }
     return ans;
 }
