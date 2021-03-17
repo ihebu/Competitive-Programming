830af0962b31d40c105998eab313b3b93b8f22cb
Iheb Haboubi <iheb.haboubi56@gmail.com>
2021-03-17 22:30:11 +0100

    overload multiplication operator

--- Math/matrix.cpp
+++ Math/matrix.cpp
@@ -14,43 +14,39 @@ typedef pair<ll, ll> pll;
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
 
-#define matrix vector<vll>
+typedef vector<vll> matrix;
 const ll MOD = 1000000007;
 
-// return the identity matrix of size n x n
+// identity matrix of size n x n
 matrix id(int n)
 {
     matrix a(n, vll(n, 0));
-    for (int i = 0; i < n; i++)
-        a[i][i] = 1;
+    for (int i = 0; i < n; i++) a[i][i] = 1;
     return a;
 }
 
-// matrix multiplication
-matrix mul(matrix a, matrix b)
+matrix operator*(matrix a, matrix b)
 {
     int n = a.size(), p = a[0].size(), m = b[0].size();
     matrix c(n, vll(m, 0));
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
         {
-            for (int k = 0; k < p; k++)
-                c[i][j] += (a[i][k] * b[k][j]) % MOD;
+            for (int k = 0; k < p; k++) c[i][j] += (a[i][k] * b[k][j]) % MOD;
             c[i][j] %= MOD;
         }
     return c;
 }
 
-// fast exponentiation
-matrix fast(matrix a, ll k)
+// binary exponentiation of a square matrix
+matrix modpow(matrix a, ll k)
 {
     int n = a.size();
     matrix ans = id(n);
     while (k)
     {
-        if (k & 1)
-            ans = mul(ans, a);
-        a = mul(a, a);
+        if (k & 1) ans = ans * a;
+        a = a * a;
         k >>= 1;
     }
     return ans;
