907fabfd654b4b4c9dfd62d9ad05aaa167899821
Iheb Haboubi <iheb.haboubi56@gmail.com>
2021-04-06 14:44:42 +0100

    Use struct

--- Graphs/dijkstra.cpp
+++ Graphs/dijkstra.cpp
@@ -1,39 +1,42 @@
-const ll INF = 1e16;
-
-int main()
+struct dijkstra
 {
-    int n, m;
-    cin >> n >> m;
-    vector<vector<pii>> adj(n + 1);
-    for (int i = 0; i < m; i++)
+    const ll INF = 1e18;
+    vector<vector<pii>> adj;
+    priority_queue<pll, vector<pll>, greater<pll>> pq;
+    vll dist;
+    vi p;
+    int s;
+
+    dijkstra(int n, int s) : s(s)
     {
-        int a, b, c;
-        cin >> a >> b >> c;
-        adj[a].push_back({c, b});
+        adj.resize(n + 1);
+        p.resize(n + 1);
+        dist.resize(n + 1, INF);
     }
 
-    vll dist(n + 1, INF), p(n + 1, -1);
-    priority_queue<pll, vector<pll>, greater<pll>> pq;
+    void add(int a, int b, int c) { adj[a].push_back({b, c}); }
 
-    dist[1] = 0;
-    pq.push({0, 1});
-
-    while (!pq.empty())
+    void run()
     {
-        ll a, x;
-        tie(x, a) = pq.top();
-        pq.pop();
-        if (x != dist[a]) continue;
-        for (auto e : adj[a])
+        dist[s] = 0;
+        pq.push({0, s});
+        while (!pq.empty())
         {
-            ll b, c;
-            tie(c, b) = e;
-            if (dist[a] + c < dist[b])
+            ll a, x;
+            tie(x, a) = pq.top();
+            pq.pop();
+            if (x != dist[a]) continue;
+            for (auto e : adj[a])
             {
-                dist[b] = dist[a] + c;
-                p[b] = a;
-                pq.push({dist[b], b});
+                ll b, c;
+                tie(b, c) = e;
+                if (dist[a] + c < dist[b])
+                {
+                    dist[b] = dist[a] + c;
+                    p[b] = a;
+                    pq.push({dist[b], b});
+                }
             }
         }
     }
-}
+};
