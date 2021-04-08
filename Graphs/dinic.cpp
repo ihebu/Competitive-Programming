7f88b7d2da4581683ff266560f7b4a4d8cf356b7
Iheb Haboubi <iheb.haboubi56@gmail.com>
2021-04-08 15:47:37 +0100

    Update structure

--- Graphs/dinic.cpp
+++ Graphs/dinic.cpp
@@ -1,9 +1,15 @@
 struct dinic
 {
+    struct edge
+    {
+        int a, b;
+        ll c;
+        edge(int a, int b, ll c) : a(a), b(b), c(c) {}
+    };
     int s, t;
     vi level;
     vector<vi> adj;
-    vector<pair<int, ll>> edges;
+    vector<edge> edges;
     queue<int> q;
     int m = 0;
     const ll INF = 1e18;
@@ -16,8 +22,8 @@ struct dinic
 
     void add(int a, int b, ll c)
     {
-        edges.push_back({b, c});
-        edges.push_back({a, 0});
+        edges.emplace_back(a, b, c);
+        edges.emplace_back(b, a, 0);
         adj[a].push_back(m++);
         adj[b].push_back(m++);
     }
@@ -33,9 +39,8 @@ struct dinic
             q.pop();
             for (int id : adj[a])
             {
-                int b;
-                ll c;
-                tie(b, c) = edges[id];
+                int b = edges[id].b;
+                ll c = edges[id].c;
                 if (c == 0 || level[b] != -1) continue;
                 level[b] = level[a] + 1;
                 q.push(b);
@@ -49,14 +54,13 @@ struct dinic
         if (a == t || flow == 0) return flow;
         for (int id : adj[a])
         {
-            int b;
-            ll c;
-            tie(b, c) = edges[id];
+            int b = edges[id].b;
+            ll c = edges[id].c;
             if ((level[b] <= level[a]) || c == 0) continue;
             ll f = dfs(b, min(flow, c));
             if (f == 0) continue;
-            edges[id ^ 1].second += f;
-            edges[id].second -= f;
+            edges[id].c -= f;
+            edges[id ^ 1].c += f;
             return f;
         }
         return 0;
